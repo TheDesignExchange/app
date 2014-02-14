87414323bd5368d0a97366fb8b476a6eb1475784
Candy Chang <candyichang@gmail.com>
2014-02-14 01:00:58 -0800

    method category: lineage format

--- app/models/method_category.rb
+++ app/models/method_category.rb
@@ -8,9 +8,52 @@
 #  updated_at :datetime         not null
 #
 
+# STUFF TO CONSIDER:
+# currently for add/remove: just removing child from lineage list.
+# --if the child gets completely deleted beforehand, then need to change
+#   the argument that gets passed (instead of method category, need the id only or something)
+# when a method category is created, does the parent id and lineage need to get filled out?
+# or should there be some method here that fills that in?
+# if a "root" method is added how is everything going to get filled? copy-paste lineage list from child?
+
 class MethodCategory < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 255,
             too_long: "%{count} is the maximum character length."}
+
+  def parent
+    return MethodCategory.find(self.parent_id)
+  end
+
+  def immediatechildren
+    return MethodCategory.where(parent_id: self.id)
+  end
+
+  def children(depth = -1)
+    to_return = Array.new
+    child_list = self.lineage
+    child_list.split(",").each do |id|
+      child = MethodCategory.find(id.to_i)
+      if child && (depth < 0 || child.depth >= depth)
+        to_return << child
+      end
+    end
+    return to_return
+  end
+
+  def addchild(child)
+    if MethodCategory.exists?(child.id)
+      self.lineage << ", " << child.id.to_s
+    end
+  end
+
+  def removechild(child)
+    to_remove = MethodCategory.find(child.id)
+    if to_remove
+      value = child.id.to_s
+      regex = /(?:[^\d]|^)#{value}(?:[^\d]|$)/
+      self.lineage.slice!(regex)
+    end
+  end
 
   has_many :categorizations, dependent: :destroy
   has_many :design_methods, through: :categorizations
--- db/migrate/20130819014901_create_method_categories.rb
+++ db/migrate/20130819014901_create_method_categories.rb
@@ -2,6 +2,9 @@ class CreateMethodCategories < ActiveRecord::Migration
   def change
     create_table :method_categories do |t|
       t.string :name
+      t.integer :parent_id
+      t.string :lineage
+      t.integer :depth
 
       t.timestamps
     end
