c87cb9f88fa412bfd52483a310b24c346acd0989
Candy Chang <candyichang@gmail.com>
2014-02-27 19:04:19 -0800

    changed MethodCategory to use getter/setter methods for distance and type of relation. before_destroy still does not want to work.

--- app/models/method_category.rb
+++ app/models/method_category.rb
@@ -9,17 +9,17 @@
 #
 
 class MethodCategory < ActiveRecord::Base
-  before_destroy :changedistance
+  before_destroy :change_distance
   validates :name, presence: true, length: { maximum: 255,
             too_long: "%{count} is the maximum character length."}
 
   #Retrieves all children with a distance <= DEPTH. Default is immediate children (depth = 1).
   #To get all children, use self.children instead.
-  def getchildren(depth = 1)
+  def get_children(depth = 1)
     to_return = Array.new
-    self.children.each do |c|
-      if c.distance <= depth
-        to_return << c
+    self.children.each do |child|
+      if self.distance_from(child) <= depth
+        to_return << child
       end
     end
     return to_return
@@ -27,91 +27,113 @@ class MethodCategory < ActiveRecord::Base
 
   #Retrieves all parents with a distance <= DEPTH. Default is immediate parents (depth = 1).
   #To get all parents, use self.parents instead.
-  def getparents(depth = 1)
+  def get_parents(depth = 1)
     to_return = Array.new
-    self.parents.each do |c|
-      if c.distance <= depth
-        to_return << c
+    self.parents.each do |parent|
+      if parent.distance_from(self) <= depth
+        to_return << parent
       end
     end
     return to_return
   end
 
   #Creates relation between self and CHILD, modifies distances from any parents of SELF to CHILD
-  def addchild(child, distance = 1, type = "subclass")
-    McRelations.find_or_create_by(parent_id: self.id, child_id: child.id) do |r|
-      r.distance = distance
-      r.description = type
+  def add_child(child, distance = 1, type = "subclass")
+    if !self.children.exists?(child)
+      self.children << child
     end
+    self.update_all_fields(child, distance, type)
 
-    self.ancestors.each do |a|
-      new_dist = a.distance + distance
-      McRelations.find_or_create_by(parent_id: a.parent_id, child_id: child.id) do |u|
-        u.distance = new_dist
-        u.description = type
+    self.parents.each do |parent|
+      if !parent.children.exists?(child)
+        parent.children << child
       end
+      offset = parent.distance_from(self)
+      parent.update_all_fields(child, distance + offset, type)
     end
   end
 
   #Creates relation between self and PARENT, modifies distances from any children of self to PARENT
-  def addparent(parent, distance = 1, type = "subclass")
-    McRelations.find_or_create_by(parent_id: parent.id, child_id: child.id) do |r|
-      r.distance = distance
-      r.description = description
+  def add_parent(parent, distance = 1, type = "subclass")
+    if !self.parents.exists?(parent)
+      self.parents << parent
     end
+    self.update_all_fields(parent, distance, type)
 
-    self.descendants.each do |c|
-      new_dist = c.distance + distance
-      McRelations.find_or_create_by(parent_id: c.id, child_id: self.id)do |u|
-        u.distance = new_dist
-        u.description = type
+    self.children.each do |child|
+      if !child.parents.exists?(parent)
+        child.parents << parent
       end
+      offset = self.distance_from(child)
+      child.update_all_fields(parent, distance + offset, type)
     end
   end
 
   # Removes CHILD from SELF's list of children. Modifies distances from any children or parents of self.
-  def removechild(child)
+  def remove_child(child)
     self.children.destroy(child)
   end
 
   # Removes parent from SELF's list of parents. Modifies distances from any parents or children of self.
-  def removeparent(parent)
+  def remove_parent(parent)
     self.parents.destroy(parent)
   end
 
-  def changedistance
-    self.descendants.each do |d|
-      self.ancestors.each do |a|
-        dist = McRelations.find_by(parent_id: a.parent_id, child_id: d.child_id)
-        if !dist.empty?
-          if dist.distance > d.distance
-            dist.distance -= d.distance
-            dist.save
-          end
+  def change_distance
+    self.parents.each do |parent|
+      self.children.each do |child|
+        old_dist = parent.distance_from(child)
+        if old_dist
+          offset = parent.distance_from(self)
+          parent.update_distance(child, old_dist - offset)
         end
       end
     end
   end
 
-  def distance(other, parent = true)
-    if parent
-      relation = McRelations.find_by(parent_id: self.id, child_id: other.id)
-    else
-      relation = McRelations.find_by(parent_id: other.id, child_id: self.id)
-    end
+  # Gets the distance from SELF to OTHER, where SELF is the parent in the relationship.
+  def distance_from(other)
+    relation = McRelations.where(parent_id: self.id, child_id: other.id).first
+    if relation
       return relation.distance
+    end
   end
 
-  def updatedist(other, distance, parent = true)
-    old_distance = self.distance(other, parent)
-    if parent
-      update_relation = McRelations.find_by(parent_id: self.id, child_id: other.id)
-    else
-      update_relation = McRelations.find_by(parent_id: other.id, child_id: self.id)
-    end
+  # Updates the distance from SELF to OTHER, where SELF is the parent in the relationship.
+  def update_distance(other, distance)
+    update_relation = McRelations.where(parent_id: self.id, child_id: other.id).first
+    if update_relation
       update_relation.distance = distance
       update_relation.save
+      return update_relation.distance
     end
+  end
+
+  # Returns the type of relation between SELF and OTHER, where SELF is the parent.
+  def relation_type(other)
+    relation = McRelations.where(parent_id: self.id, child_id: other.id).first
+    if relation
+      return relation.description
+    end
+  end
+
+  # Updates the type of relation between SELF and OTHER, where SELF is the parent.
+  def update_relation_type(other, type)
+    update_relation = McRelations.where(parent_id: self.id, child_id: other.id).first
+    if update_relation
+      update_relation.description = type
+      update_relation.save
+      return update_relation.description
+    end
+  end
+
+  # Updates DISTANCE and TYPE of relation for SELF and OTHER, where SELF is the parent.
+  def update_all_fields(other, distance, type)
+    update_relation = McRelations.where(parent_id: self.id, child_id: other.id).first
+    update_relation.description = type
+    update_relation.distance = distance
+    update_relation.save
+  end
 
   has_many :descendants,  :class_name => "McRelations",
                           :foreign_key => "parent_id",
