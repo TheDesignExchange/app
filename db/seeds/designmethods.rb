8042784d90309e1783cfd60d7d75bf595f0f0805
Candy Chang <candyichang@gmail.com>
2015-01-18 00:19:04 -0800

    design method seed updated

--- db/seeds/designmethods.rb
+++ db/seeds/designmethods.rb
@@ -27,111 +27,7 @@ CHAR_ROW = 1
 METHOD_ROW = 3
 VARIATION_INDEX = 7
 
-
-# def load_characteristics(category, sheet)
-#   p "=================== LOAD GROUPS & CHARACTERISTICS  ====================="
-#   i = CHARACTERISTIC_INDEX
-#   group_list = sheet.row(CHAR_ROW)
-#   char_list = sheet.row(CHAR_ROW + 1)
-#   group_name = group_list[i]
-#   characteristics = char_list[i]
-
-#   while group_name != nil
-#     g_name = group_name.to_s.strip
-#     group = CharacteristicGroup.new
-#     group.name = g_name
-#     if group.save
-#       category.characteristic_groups << group
-#       p "Added #{group.name}"
-#     end
-
-#     c_names = characteristics.split(/ *, */).each do |c_name|
-#       character = Characteristic.new
-#       character.name = c_name
-#       if character.save
-#         group.characteristics << character
-#         p "    Added #{character.name}"
-#       else
-#         p character.errors
-#       end
-#     end
-
-#     i += 1
-#     group_name = group_list[i]
-#     characteristics = char_list[i]
-#   end
-# end
-
-def load_methods(category, sheet, admin)
-
-  p "========================= LOAD DESIGN METHODS ========================="
-
-  #Load in design methods
-  fields = Hash.new
-  sheet.each METHOD_ROW do |row|
-    fields[:name] = row[METHOD_INDEX].to_s.strip
-
-    if fields[:name].empty?
-      break
-    end
-
-    fields[:overview] = row[METHOD_INDEX + 1].to_s.strip
-    fields[:process] = "default"
-    aka = row[METHOD_INDEX + 2].to_s.strip
-    image = row[METHOD_INDEX + 3].to_s.strip
-
-    design_method = DesignMethod.new(fields)
-    design_method.owner = admin
-    design_method.aka = aka
-    design_method.main_image = image
-
-    if !design_method.save
-      p "Error while creating a design method: "
-      design_method.errors.full_messages.each do |message|
-        p "\t#{message}"
-      end
-    else
-      p "Added #{design_method.name}"
-    
-      #Load in characteristics 
-      j = CHARACTERISTIC_INDEX
-      characteristics = row[j]
-      group_row = sheet.row(CHAR_ROW)
-
-      while characteristics != nil
-        group_name = group_row[j].to_s.strip
-        group = CharacteristicGroup.where(name: group_name).first_or_create!
-        characteristics.split(/[, *]/).each do |char_name|
-          if !char_name.blank?
-            characteristic = Characteristic.where({name: char_name, characteristic_group_id: group.id}).first_or_create!
-            if characteristic && !design_method.characteristics.include?(characteristic)
-              design_method.characteristics << characteristic
-              p "    Added #{characteristic.name}"
-            end
-          end
-        end
-
-        j += 1
-        characteristics = row[j]
-      end
-
-      #Load in citations
-      citations = row[METHOD_INDEX + 4]
-      if citations
-        citations.split(/, *\n/).each do |cit|
-          if !cit.blank?
-          citation = Citation.where(text: cit).first_or_create!
-            if !design_method.citations.include?(citation)
-              design_method.citations << citation
-              p "    Added #{citation.text}"
-            end
-          end
-        end
-      end
-    end
-  end
-
-  #Load variations in a second pass
+def load_variations(sheet)
   sheet.each METHOD_ROW do |row|
     parents = row[VARIATION_INDEX].to_s.strip
     if parents.empty?
@@ -142,14 +38,87 @@ def load_methods(category, sheet, admin)
 
       parents.split(/, */).each do |parent|
         parent_method = DesignMethod.where(name: parent).first
-        if variation_method && parent_method
+        if variation && parent_method
           parent_method.variations << variation
-          p "    Added: #{variant_name} is variation of #{parent_name}"
+          p "    Added: #{variant_name} is variation of #{parent}"
         end
       end
     end
   end
+end
 
+def load_methods(category, sheet, admin)
+
+  p "========================= LOAD DESIGN METHODS ========================="
+
+  #Load in design methods
+  fields = Hash.new
+  sheet.each METHOD_ROW do |row|
+    name = row[METHOD_INDEX].to_s.strip
+
+    if name.empty?
+      break
+    elsif DesignMethod.exists?(name: name)
+      design_method = DesignMethod.where(name: name).first
+    else
+      fields[:name] = name
+      fields[:overview] = row[METHOD_INDEX + 1].to_s.strip
+      fields[:process] = "default"
+      aka = row[METHOD_INDEX + 2].to_s.strip
+      image = row[METHOD_INDEX + 3].to_s.strip
+
+      design_method = DesignMethod.new(fields)
+      design_method.owner = admin
+      design_method.aka = aka
+      design_method.main_image = image
+
+      if !design_method.save
+        p "Error while creating a design method: #{fields[:name]} "
+        design_method.errors.full_messages.each do |message|
+          p "\t#{message}"
+        end
+        next
+      end
+    end
+
+    p "Added #{design_method.name}"
+  
+    #Load in characteristics 
+    j = CHARACTERISTIC_INDEX
+    characteristics = row[j]
+    group_row = sheet.row(CHAR_ROW)
+
+    while characteristics != nil
+      group_name = group_row[j].to_s.strip
+      group = CharacteristicGroup.where(name: group_name).first_or_create!
+      characteristics.split(/, *\n*/).each do |char_name|
+        if !char_name.blank?
+          characteristic = Characteristic.where({name: char_name, characteristic_group_id: group.id}).first_or_create!
+          if characteristic && !design_method.characteristics.include?(characteristic)
+            design_method.characteristics << characteristic
+            p "    Added #{characteristic.name}"
+          end
+        end
+      end
+
+      j += 1
+      characteristics = row[j]
+    end
+
+    #Load in citations
+    citations = row[METHOD_INDEX + 4]
+    if citations
+      citations.split(/, *\n/).each do |cit|
+        if !cit.blank?
+        citation = Citation.where(text: cit).first_or_create!
+          if !design_method.citations.include?(citation)
+            design_method.citations << citation
+            p "    Added #{citation.text}"
+          end
+        end
+      end
+    end
+  end
 end
 
 # Create five basic method categories
@@ -189,6 +158,7 @@ categories.each do |cat|
 
   # load_characteristics(cat, sheet)
   load_methods(cat, sheet, admin)
+  load_variations(sheet)
 end
 
 
