f62a583312b5fbd11e0e929b126b0035bff86eb4
Cesar Torres <cearto@berkeley.edu>
2015-02-19 12:09:50 -0800

    reseed images script

---
+++ db/reseed_images.rb
@@ -0,0 +1,46 @@
+# CASESTUDY
+
+ # casestudies = [[1, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/1/Screen_Shot_2014-09-01_at_3.09.03_PM.png"], [2, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/2/inception.png"], [3, nil], [4, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/4/Screen_Shot_2014-08-28_at_6.22.34_PM.png"], [5, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/5/Screen_Shot_2014-08-28_at_6.25.22_PM.png"], [6, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/6/Screen_Shot_2014-08-25_at_4.47.08_PM.png"], [7, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/7/Screen_Shot_2014-08-28_at_6.29.50_PM.png"], [8, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/8/Boat-Shot.jpg"], [9, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/9/Unilever-on-tea-Available-evidence-supports-tea-and-tea-ingredients-for-mood-and-performance-benefits_strict_xxl.jpg"], [10, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/10/imgres.jpg"], [11, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/11/imgres-1.jpg"], [12, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/12/url.jpg"], [13, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/13/Screen_Shot_2014-09-01_at_3.56.49_PM.png"], [14, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/14/Screen_Shot_2014-09-01_at_4.05.55_PM.png"], [15, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/15/Screen_Shot_2014-09-01_at_4.17.36_PM.png"], [16, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/16/Screen_Shot_2014-09-01_at_4.18.44_PM.png"], [17, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/17/textbook.jpg"], [18, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/18/gum.jpg"], [19, nil], [20, nil], [21, nil], [22, nil], [23, nil], [24, nil], [25, nil], [26, nil], [27, nil], [28, nil], [29, nil], [30, nil], [31, nil], [32, nil], [33, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/33/CS_Samsung_ID_1010.jpg"], [34, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/34/primasee.jpg"], [35, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/35/pringles.jpg"], [36, nil], [37, nil], [38, nil], [39, nil], [40, nil], [41, nil], [42, nil], [43, nil], [44, nil], [45, nil], [46, nil], [47, nil], [48, nil], [49, nil], [50, nil], [51, nil], [52, nil], [53, nil], [54, nil], [55, nil], [56, nil], [57, nil], [58, nil], [59, nil], [60, nil], [61, nil], [62, nil], [63, nil], [64, nil], [65, nil], [66, nil], [67, nil], [68, nil], [69, nil], [70, nil], [71, nil], [72, nil], [73, nil], [74, nil], [75, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/75/pg.jpg"], [76, nil], [77, nil], [78, nil], [79, nil], [80, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/80/plex.jpg"], [81, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/81/alyx.jpg"], [82, nil], [83, nil], [84, nil], [85, nil], [86, nil], [87, nil], [88, nil], [89, nil], [90, nil], [91, nil], [92, nil], [93, nil], [94, nil], [95, nil], [96, nil], [97, nil], [98, nil], [99, nil], [100, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/100/agvera.png"], [101, nil], [102, nil], [103, nil], [104, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/104/pem.png"], [105, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/105/adobe.png"], [106, nil], [107, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/107/apnicure.jpg"], [108, nil], [109, nil], [110, nil], [111, nil], [112, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/112/wand.jpg"], [113, nil], [114, nil], [115, nil], [116, nil], [117, nil], [118, nil], [119, nil], [120, nil], [121, nil], [122, nil], [123, nil], [124, nil], [125, nil], [126, nil], [127, nil], [128, nil], [129, nil], [130, nil], [131, nil], [132, nil], [133, nil], [134, nil], [135, nil], [136, nil], [137, nil], [138, nil], [139, nil], [140, nil], [141, nil], [142, nil], [143, nil], [144, nil], [145, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/145/Adidas_Slide2_02.jpg"], [146, nil], [147, nil], [148, nil], [149, nil], [150, nil], [151, nil], [152, nil], [153, nil], [154, nil], [155, nil], [156, nil], [157, nil], [158, nil], [159, nil], [160, nil], [161, nil], [162, nil], [163, nil], [164, "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/case_study/mainImage/164/audi.jpg"], [165, nil], [166, nil], [167, nil], [168, nil], [169, nil], [170, nil]]
+
+ #  def link_images(imgs, model)
+ #    # CaseStudy.all.select(:id, :mainImage).collect{|x| [x.id, x.mainImage.current_path]}
+ #    # DesignMethod.all.select(:id, :main_image).collect{|x| [x.id, x.main_image.current_path]}.select{|x|not x[1].nil?}
+ #    imgs =     changes = []
+ #    imgs.each do |x|
+ #      if not x[1].nil?
+ #        id = x[0]
+ #        path = x[1].gsub("/home/keien/dev/design_exchange/Code/DesExSearch/", "")
+ #        changes << upload_direct(model, id, path)
+
+ #      end
+ #    end
+ #    print changes
+ #  end
+
+ #  def upload_direct(model, id, path)  
+ #    a = CaseStudy.find(id)
+ #    a.mainImage = File.open(path)
+ #    return a.save!
+ #  end
+
+
+
+# METHODS
+  # def link_images_methods
+  #   dm = [["3-12-3_Brainstorm", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/2/153846297_55d1cefa8a__1_.jpg"], ["635Brainwriting", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/5/635f.png"], ["7Ps_Framework", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/6/Playbook_507-300x227.png"], ["AEIOU", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/7/66e18e9bb1638be8d924bcfd41db41d1.jpg"], ["Abstraction", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/10/download.jpeg"], ["Action_Planning", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/11/action-plan.png"], ["Action_Research", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/12/SystemsModelofActionResearchProcess.png"], ["Active_Listening", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/13/19e0016.jpg"], ["Heuristic_Evaluation", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/139/heuristics_points2.png"], ["Contextual Inquiry", "/home/keien/dev/design_exchange/Code/DesExSearch/public/uploads/design_method/main_image/417/Apprenticeship.jpg"]]
+  #   changes = []
+  #   dm.each do |x|
+  #     name = x[0]
+  #     path = x[1].gsub("/home/keien/dev/design_exchange/Code/DesExSearch/", "")
+  #     method = DesignMethod.find(:first, :conditions => ["lower(name) = ?", name.downcase.gsub(" ", "_")]) 
+  #     if not method.nil?
+  #        changes << upload_direct(method, path)
+  #     end 
+  #   end
+  #   print changes
+  # end
+
+  # def upload_direct(method, path)  
+  #   method.main_image = File.open(path)
+  #   return method.save!
+  # end
