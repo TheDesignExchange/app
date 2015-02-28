6152a5e529a08e365eca595c98c936776f65987f
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2015-02-28 13:46:00 -0800

    add json output for design methods page

--- app/controllers/design_methods_controller.rb
+++ app/controllers/design_methods_controller.rb
@@ -3,7 +3,11 @@ class DesignMethodsController < ApplicationController
   def index
     @design_methods = DesignMethod.where("overview != ?", "No overview available" ).take(24)
     @design_methods = DesignMethod.take(24)
-    render :layout => "wide"
+    @design_methods_all = DesignMethod.all
+    respond_to do |format|
+      format.html { render :layout => "wide" }
+      format.json { render json: @design_methods_all} 
+    end
   end
 
   def new
