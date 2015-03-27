58ba5b548409fe937aaa251a84763fc0bdd48070
Keien Ohta <keienohta@gmail.com>
2015-03-26 20:20:01 -0700

    Errors in tag and mcs

--- test/functional/tags_controller_test.rb
+++ test/functional/tags_controller_test.rb
@@ -3,8 +3,6 @@ require 'test_helper'
 class TagsControllerTest < ActionController::TestCase
   setup do
   	@tag = tags(:one)
-  	@user = users(:one)
-  	sign_in @user
   end 
 
   test "should create tag" do
