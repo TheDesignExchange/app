c0f33695e71832e09e4997655e6ea0ea69323f8d
Keien Ohta <keienohta@gmail.com>
2015-05-18 15:37:21 -0700

    Uncommented validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,15 +13,15 @@
 class Company < ActiveRecord::Base
 	attr_accessible :name, :email, :domain, :id
 	before_save { self.email = email.downcase }
-  	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	# validates :email, 
-  	# 								presence:   true,
-   #                  format:     { with: VALID_EMAIL_REGEX },
-   #                  uniqueness: { case_sensitive: false }
+  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, 
+  									presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
 	has_many :case_studies
 	has_many :contacts
 
-	#validates :name, uniqueness: true
-	#validates_presence_of :name, :domain, :email
+	validates :name, uniqueness: true
+	validates_presence_of :name, :domain, :email
 end
