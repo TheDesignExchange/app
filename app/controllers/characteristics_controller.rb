bf14fe5c7dfbba0105e6e0bd998dc3dfa430b4d9
Keien Ohta <keienohta@gmail.com>
2015-06-18 10:46:48 -0700

    Filter criteria unknown when clicking from design method

--- app/controllers/characteristics_controller.rb
+++ app/controllers/characteristics_controller.rb
@@ -1,17 +1,27 @@
 class CharacteristicsController < ApplicationController
   def show 
-    @same_characteristic_design_methods = DesignMethod.where("characteristic = ?", params[:characteristic_id])
-    @characteristic = Characteristic.where(id: params[:characteristic_id])
-    @design_methods = DesignMethod.where("overview != ?", "No overview available" )
+    if params[:id]
+      id = params[:id].to_i
+      @characteristic = Characteristic.where(id: params[:id]).first
+      @design_methods = @characteristic.design_methods   
+      @search_filter_hash = MethodCategory.all
+      @design_methods_all = DesignMethod.all
+      respond_to do |format|  
+        format.html { render :layout => "wide" }
+        format.json { render json: @design_methods_all} 
+      end  
+    else 
+      @design_methods = DesignMethod.where("overview != ?", "No overview available" )
       # .take(24)
       # @design_methods = DesignMethod.take(24)
        # Filter bar needs
       @search_filter_hash = MethodCategory.all
       @design_methods_all = DesignMethod.all
-      respond_to do |format|
+      respond_to do |format|  
         format.html { render :layout => "wide" }
         format.json { render json: @design_methods_all} 
       end
+    end 
   end 
 end 
 
--- app/controllers/design_methods_controller.rb
+++ app/controllers/design_methods_controller.rb
@@ -3,10 +3,6 @@ class DesignMethodsController < ApplicationController
   before_action :create_as_signed_in_user, only: [:create, :new]
 
   def index
-    if params[:characteristic_id]
-      @characteristic = Characteristic.where(id: params[:characteristic_id])
-      @design_methods = @characteristic.design_methods      
-    else 
       @design_methods = DesignMethod.where("overview != ?", "No overview available" )
       # .take(24)
       # @design_methods = DesignMethod.take(24)
@@ -17,7 +13,6 @@ class DesignMethodsController < ApplicationController
         format.html { render :layout => "wide" }
         format.json { render json: @design_methods_all} 
       end
-    end 
   end
 
   def new
