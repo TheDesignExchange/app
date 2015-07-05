7072248c0ae8f839f4ea447ccc15c14259565590
Sean Zhu <sean.zhu@berkeley.edu>
2015-07-05 04:29:30 -0700

    Make URLs prettier
    `/design_methods/4` -> `/methods/4`
    `/case_studies/6` -> `/studies/6`
    
    Old-style URLs should redirect to new ones.

--- config/routes.rb
+++ config/routes.rb
@@ -14,23 +14,34 @@ DesignExchange::Application.routes.draw do
   resources :method_case_studies
   root "application#index"
 
-  resources :case_studies do
-    collection do 
-      get "related_methods", to: "case_studies#related_methods"
+  case_studies = "studies"
+  resources "#{case_studies}", as: :case_studies, controller: :case_studies do
+    collection do
+      get "related", to: "case_studies#related_methods"
+      match "related_methods" => redirect("related"), via: [:get, :post]
       get "search/:query", to: "application#search"
       get "search", to: "application#search", :as => "search"
     end
-    member do 
-      get "related_methods", to: "case_studies#related_methods", :as => "related_methods"
+    member do
+      get "related", to: "case_studies#related_methods", :as => "related_methods"
+      match "related_methods" => redirect("related"), via: [:get, :post]
     end
   end
-  resources :design_methods do
-    collection do 
+  # TODO: legacy redirect; remove these eventually
+  match "case_studies" => redirect("#{case_studies}"), via: [:get, :post]
+  match "case_studies/:suffix" => redirect("#{case_studies}/%{suffix}"), via: [:get, :post]
+
+  design_methods = "methods"
+  resources "#{design_methods}", as: :design_methods, controller: :design_methods do
+    collection do
       get "search/:query", to: "application#search"
       get "search", to: "application#search", :as => "search"
       get "method_category/:category_id",  to: "application#search", :as => "search_category"
     end
   end
+  # TODO: legacy redirect; remove these eventually
+  match "design_methods" => redirect("#{design_methods}"), via: [:get, :post]
+  match "design_methods/:suffix" => redirect("#{design_methods}/%{suffix}"), via: [:get, :post]
 
   resources :discussions do
     collection do 
