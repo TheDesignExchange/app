a19aa263f44ae41f4506a9dcada13e6a20c62add
jennyfromtheweb <jennyzhcl@yahoo.com>
2015-10-15 15:11:11 -0700

    fixed issue of error messages not flashing when trying to add or create a method or study when not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
+  add_flash_types :success, :warning, :danger, :info
 
 
     # The DesignExchange home page.
