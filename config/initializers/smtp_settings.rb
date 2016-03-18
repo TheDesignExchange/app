55e152c9493dd42626aadf44e054c5a734341949
Greg Marcil <grmarcil@gmail.com>
2016-03-18 00:00:41 -0700

    Add key management for mailgun

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,4 +1,8 @@
-# Replace the following with actual domain and key management system
+if Rails.env.development? or Rails.env.test?
+  mailgun_token = 'fde364a19893533a938a9acaadf98890' #test account password
+else
+  mailgun_token = ENV['MAILGUN_API_TOKEN']
+end
 
 ActionMailer::Base.smtp_settings = {
     :enable_starttls_auto => true,
@@ -7,5 +11,5 @@ ActionMailer::Base.smtp_settings = {
     :domain         => 'mailgun.org',
     :authentication => :plain,
     :user_name      => 'postmaster@sandbox3419534bf0ee465fb886bc9f1ada4faa.mailgun.org',
-    :password       => 'fde364a19893533a938a9acaadf98890'
+    :password       => mailgun_token
   }
