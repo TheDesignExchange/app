8e759523077efa6cc43631c0bbec8b1c756dd275
Greg Marcil <grmarcil@gmail.com>
2016-03-17 18:47:31 -0700

    Update note for correct url

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,11 +1,11 @@
 class FeedbackMailer < ActionMailer::Base
-	# replace with admin@designexchange.org
+	# replace with admin@thedesignexchange.org
   default :to => "jennyli@berkeley.edu"
 
 	def message_me(msg)
 		@msg = msg
 		if @msg.email.empty?
-			# replace with admin@designexchange.org
+			# replace with admin@thedesignexchange.org
 			@msg.email = "postmaster@sandbox3419534bf0ee465fb886bc9f1ada4faa.mailgun.org"
 		end
 		mail from: @msg.email, subject: "[Feedback] " + @msg.subject, body: @msg.content
