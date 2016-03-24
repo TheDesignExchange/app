2cf0de2f96d7e849c04d5115385d5e5c0a4d2d65
jennyfromtheweb <jennyzhcl@yahoo.com>
2016-03-24 00:13:37 -0700

    added env vars for captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,12 @@
 # config/initializers/recaptcha.rb
-Recaptcha.configure do |config|
-  config.public_key  = 'xxx'
-  config.private_key = 'yyy'
-  # Uncomment the following line if you are using a proxy server:
-  # config.proxy = 'http://myproxy.com.au:8080'
+if Rails.env.development? or Rails.env.test?
+  Recaptcha.configure do |config|
+    config.public_key  = 'xxx'
+    config.private_key = 'yyy'
+  end
+else
+  Recaptcha.configure do |config|
+    config.public_key  = ENV['CAPTCHA_SITE_KEY']
+    config.private_key = ENV['CAPTCHA_SECRET_KEY']
+  end
 end
