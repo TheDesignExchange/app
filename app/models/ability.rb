9a35b33e5977331acd8a961dfa00cc3bb93398af
Greg Marcil <grmarcil@gmail.com>
2016-04-15 00:05:47 -0700

    Give admins the ability to update users and remove the ridiculous unused create method in the users controller that instantiates a user as a design menthod.... never used because devise handles new user registrations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -38,6 +38,7 @@ class Ability
     if user.admin?
       # TODO this should be locked down much further since :manage is a
       # catch-all and could lead to security regressions
+      can :update, User
       can :manage, [CaseStudy, Characteristic, CharacteristicGroup, Citation,
                     Company, Contact, DesignMethod, MethodCaseStudy,
                     MethodCategory, MethodCharacteristic, MethodCitation, Tag]
