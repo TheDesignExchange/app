c028ddbb25d4a281e879a1fd70bf355af35d7432
Tina Huang <thuang@berkeley.edu>
2016-06-23 16:17:51 -0700

    trying to resolve duplicate column issue

--- db/migrate/20160613222033_add_case_study_id_to_method_collection.rb
+++ db/migrate/20160613222033_add_case_study_id_to_method_collection.rb
@@ -1,5 +1,10 @@
 class AddCaseStudyIdToMethodCollection < ActiveRecord::Migration
   def change
-  	add_column :method_collections, :case_study_id, :integer
+  	if column_exists? :method_collections, :case_study_id
+      remove_column :method_collections, :case_study_id
+    else
+      add_column :method_collections, :case_study_id, :integer
+    end
+  	
   end
 end
