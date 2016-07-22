3711cbf530dc7771e2b33d253f12b1c8d5142624
Tina Huang <thuang@berkeley.edu>
2016-07-22 16:04:58 -0700

    trying again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                     # required
   }
+  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
   config.fog_directory  = 'directory'                             # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
