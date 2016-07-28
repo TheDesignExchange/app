193b14ace6bf66359a29a14a6126e0030f8432c8
Tina Huang <thuang@berkeley.edu>
2016-07-28 13:48:49 -0700

    try removing columns instead

--- db/migrate/20160728190826_add_picture_and_picture_url_to_case_studies.rb
+++ db/migrate/20160728190826_add_picture_and_picture_url_to_case_studies.rb
@@ -1,6 +1,9 @@
 class AddPictureAndPictureUrlToCaseStudies < ActiveRecord::Migration
   def change
-    add_column :case_studies, :picture, :integer
-    add_column :case_studies, :picture_url, :string
+    #add_column :case_studies, :picture, :integer
+    #add_column :case_studies, :picture_url, :string
+
+    remove_column :case_studies, :picture, :integer
+    remove_column :case_studies, :picture_url, :integer
   end
 end
