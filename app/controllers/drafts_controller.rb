f4e1852639bd70429caad01e86a3adf0bc73d148
James Jiang <jmendre@berkeley.edu>
2016-09-15 17:50:28 -0700

    method on draft page

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -1,5 +1,9 @@
 class DraftsController < ApplicationController
 	def index
+	  @design_methods = DesignMethod.all
+
+      @search_filter_hash = MethodCategory.order(:process_order)
+      @design_methods_all = DesignMethod.all
       render :layout => "custom"
     end
 end
