b36645895fb40984ecb9fc32f50971fab215cc9d
James Jiang <jmendre@berkeley.edu>
2016-09-19 18:16:50 -0700

    suggestions box

--- app/models/case_study.rb
+++ app/models/case_study.rb
@@ -37,7 +37,7 @@ class CaseStudy < ActiveRecord::Base
                   :social_setting, :overview, :customer_is_user, :remote_project,
                   :company_id, :num_of_designers, :num_of_users, :overview, :time_period, :time_unit,
                   :resource, :process, :problem, :outcome, :design_method_ids, :hidden,
-                  :picture, :picture_url, :completion_score, :draft
+                  :picture, :picture_url, :completion_score, :draft, :suggestions
 
   belongs_to :company
   has_many :contacts, :through => :company
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916012240) do
+ActiveRecord::Schema.define(version: 20160920005052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20160916012240) do
     t.boolean  "draft",             default: false
     t.integer  "owner_id"
     t.boolean  "ready"
+    t.string   "suggestions"
   end
 
   create_table "characteristic_groups", force: true do |t|
@@ -198,6 +199,7 @@ ActiveRecord::Schema.define(version: 20160916012240) do
     t.string   "video_two_caption"
     t.boolean  "draft",                    default: false
     t.boolean  "ready"
+    t.string   "suggestions"
   end
 
   add_index "design_methods", ["collection_id"], name: "index_design_methods_on_collection_id", using: :btree
