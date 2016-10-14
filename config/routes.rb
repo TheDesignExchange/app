68f7f82f34a4ac06cde82ac927296bf027509c0f
Alice Chin <alicechin@berkeley.edu>
2016-10-13 19:23:17 -0700

    EditedSharePageandTimeStamp

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ DesignExchange::Application.routes.draw do
   get '/administrator/change_basic', to: 'administrator#changeBasic'
   get '/design_methods/:id/clearImage', to: 'design_methods#clearImage'
   get '/design_methods/new/clearImage', to: 'design_methods#clearImage'
-  get '/coming', to: "application#coming"
+  get '/discussions', to: "application#discussions" 
   get ":action", to:"application##{:action}"
   post "search", to: "application#search"
   get "autocomplete_search", to: "application#search"
