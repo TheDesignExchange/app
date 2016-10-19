a0732dd36d96a46a0841529edee91b383b0d977e
James Jiang <jmendre@berkeley.edu>
2016-10-18 23:37:51 -0700

    invite friends

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -108,4 +108,20 @@ class UserMailer < ActionMailer::Base
       })    	    	
     end
   end
+
+  def invitation_email(email)
+    if Rails.env.development? or Rails.env.test?
+      mail({
+          :from    => 'postmaster@sandbox3419534bf0ee465fb886bc9f1ada4faa.mailgun.org',
+          :to      => email,
+          :subject => "Invitation to join theDesignExchange!",
+        })
+      else
+      mail({
+          :from    => 'postmaster@thedesignexchange.org',
+          :to      => email,
+          :subject => "Invitation to join theDesignExchange!",
+        })
+      end
+  end
 end
