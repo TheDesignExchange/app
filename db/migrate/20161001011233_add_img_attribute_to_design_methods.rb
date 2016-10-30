4c837e9b2c95b7b6040e36b2280c5b634fcbbfb9
Alice Chin <alicechin@berkeley.edu>
2016-10-29 17:39:35 -0700

    Unclaim Author/Editor DesignMethods

--- db/migrate/20161001011233_add_img_attribute_to_design_methods.rb
+++
@@ -1,5 +0,0 @@
-class AddImgAttributeToDesignMethods < ActiveRecord::Migration
-  def change
-    add_column :design_methods, :image_attribution, :string
-  end
-end
--- db/migrate/20161017033813_add_last_editor_to_case_studies.rb
+++
@@ -1,5 +0,0 @@
-class AddLastEditorToCaseStudies < ActiveRecord::Migration
-  def change
-    add_column :case_studies, :last_editor, :string
-  end
-end
