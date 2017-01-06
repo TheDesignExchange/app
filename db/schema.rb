afb5a0faf65fc7e9f5950e035ce99d885e044015
James Jiang <jmendre@berkeley.edu>
2017-01-06 16:43:35 +0800

    case study search by sector

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106075038) do
+ActiveRecord::Schema.define(version: 20170106081747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 20170106075038) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "keywords"
+    t.string   "sector"
   end
 
   create_table "characteristic_groups", force: true do |t|
