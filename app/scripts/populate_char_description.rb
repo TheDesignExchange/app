48f06da98dbd7568234334630d7e96bd9ed0492a
James Jiang <jmendre@berkeley.edu>
2017-04-18 00:56:34 -0700

    busy work

---
+++ app/scripts/populate_char_description.rb
@@ -0,0 +1,4 @@
+names = ["Mock-up", "Operational Prototype", "Production"]
+descripions = ["Phase of prototyping where prototypes are not fully functional.", 
+	"Prototype has the look and functionality of the final design",  "Ready to be produced"]
+	
